48c3fc47cf0f34d6be831fcff0587bc6ea58b35f
Mike Kruk <tamagokun@gmail.com>
2013-06-18 08:43:41 -0400

    first working monitor. also inclusion of json configuration

--- worker.go
+++ worker.go
@@ -2,15 +2,68 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"io/ioutil"
+	"encoding/json"
 	"net/http"
-	"os"
-	"github.com/robfig/cron"
 )
 
+type Config struct {
+	Wait     int
+	Timeout  int
+	Sites    []string
+}
+
+func worker(id int, jobs <-chan string, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "processing job", j)
+
+		res, err := http.Get(string(j))
+		if err != nil {
+			fmt.Println("worker", id, "site", j, "is down!")
+		}
+		fmt.Println("worker", id, "site", j, "is up")
+		results <- res.StatusCode
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func perform(config Config, jobs chan<- string, results <-chan int) {
+	for j := 0; j < len(config.Sites); j++ {
+		jobs <- config.Sites[j]
+	}
+
+	for a := 0; a < len(config.Sites); a++ {
+		<- results
+	}
+
+	select {
+		case <-time.After(time.Duration(config.Wait) * time.Second):
+			perform(config, jobs, results)
+	}
+}
+
 func main() {
-	c := cron.New()
-	c.AddFunc("0 5 * * * *",  func() { fmt.Println("Every 5 minutes") })
-	c.AddFunc("@hourly",      func() { fmt.Println("Every hour") })
-	c.AddFunc("@every 1h30m", func() { fmt.Println("Every hour thirty") })
-	c.Start()
+	jobs := make(chan string, 100)
+	results := make(chan int, 100)
+	done := make(chan bool, 1)
+
+	f, err := ioutil.ReadFile("./config.json")
+	check(err)
+	var config Config
+
+	json_err := json.Unmarshal(f, &config)
+	check(json_err)
+
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	perform(config, jobs, results)
+	<- done
 }
