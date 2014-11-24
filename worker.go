a6246548ab9fa7ed0a63f3932bdab49ac8ab338e
Mike Kruk <tamagokun@gmail.com>
2014-11-24 10:30:42 -0500

    tabs to spaces

--- worker.go
+++ worker.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"io/ioutil"
-	"encoding/json"
-	"encoding/base64"
-	"net"
-	"net/http"
-	"net/smtp"
-	"os"
+  "fmt"
+  "time"
+  "io/ioutil"
+  "encoding/json"
+  "encoding/base64"
+  "net"
+  "net/http"
+  "net/smtp"
+  "os"
 )
 
 type Config struct {
-	From      string
-	Wait      int
-	Timeout   int
-	Recipient string
-	Sites     []string
+  From      string
+  Wait      int
+  Timeout   int
+  Recipient string
+  Sites     []string
 }
 
 type Site struct {
-	Url       string
-	Status    int
+  Url       string
+  Status    int
 }
 
 func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+  if e != nil {
+    panic(e)
+  }
 }
 
 var sites = make([]Site, 0)
@@ -38,44 +38,44 @@ var config Config
  * sends email when site status changes
  */
 func notify(status string, site *Site) {
-	api_key := os.Getenv("POSTMARK_API_KEY")
-	mail_server := os.Getenv("POSTMARK_SMTP_SERVER") + ":25"
+  api_key := os.Getenv("POSTMARK_API_KEY")
+  mail_server := os.Getenv("POSTMARK_SMTP_SERVER") + ":25"
 
-	auth := smtp.CRAMMD5Auth(
-		api_key,
-		api_key,
-	)
+  auth := smtp.CRAMMD5Auth(
+    api_key,
+    api_key,
+  )
 
-	from := config.From
-	stat := http.StatusText(site.Status)
-	time := time.Now().Format("Mon Jan 02 15:04:05 2006")
-	body := "<h1>Status for " + site.Url + "</h1>"
-	body += fmt.Sprintf("<p>%s: %s (%d) @ %s</p>", status, stat, site.Status, time)
+  from := config.From
+  stat := http.StatusText(site.Status)
+  time := time.Now().Format("Mon Jan 02 15:04:05 2006")
+  body := "<h1>Status for " + site.Url + "</h1>"
+  body += fmt.Sprintf("<p>%s: %s (%d) @ %s</p>", status, stat, site.Status, time)
 
-	header := make(map[string]string)
-	header["From"] = from
-	header["To"] = config.Recipient
-	header["Subject"] = "Monitor - " + site.Url + " - " + status
-	header["Content-Type"] = "text/html; charset=\"utf-8\""
-	header["Content-Transfer-Encoding"] = "base64"
+  header := make(map[string]string)
+  header["From"] = from
+  header["To"] = config.Recipient
+  header["Subject"] = "Monitor - " + site.Url + " - " + status
+  header["Content-Type"] = "text/html; charset=\"utf-8\""
+  header["Content-Transfer-Encoding"] = "base64"
 
-	message := ""
-	for k, v := range header {
-		message += fmt.Sprintf("%s: %s\r\n", k, v)
-	}
-	message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+  message := ""
+  for k, v := range header {
+    message += fmt.Sprintf("%s: %s\r\n", k, v)
+  }
+  message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
 
-	err := smtp.SendMail(
-		mail_server,
-		auth,
-		from,
-		[]string{config.Recipient},
-		[]byte(message),
-	)
-	if err != nil {
-		fmt.Println("Unable to notify via email")
-		fmt.Println(err)
-	}
+  err := smtp.SendMail(
+    mail_server,
+    auth,
+    from,
+    []string{config.Recipient},
+    []byte(message),
+  )
+  if err != nil {
+    fmt.Println("Unable to notify via email")
+    fmt.Println(err)
+  }
 }
 
 /*
@@ -83,18 +83,18 @@ func notify(status string, site *Site) {
  * runs every config.Wait seconds.
  */
 func perform(jobs chan<- *Site, results <-chan int) {
-	for j := 0; j < len(sites); j++ {
-		jobs <- &sites[j]
-	}
+  for j := 0; j < len(sites); j++ {
+    jobs <- &sites[j]
+  }
 
-	for a := 0; a < len(sites); a++ {
-		<-results
-	}
+  for a := 0; a < len(sites); a++ {
+    <-results
+  }
 
-	select {
-		case <-time.After(time.Duration(config.Wait) * time.Second):
-			perform(jobs, results)
-	}
+  select {
+    case <-time.After(time.Duration(config.Wait) * time.Second):
+      perform(jobs, results)
+  }
 }
 
 /*
@@ -102,22 +102,22 @@ func perform(jobs chan<- *Site, results <-chan int) {
  */
 func worker(id int, jobs <-chan *Site, results chan<- int) {
 
-	for j := range jobs {
-		status := check_http_status(j.Url, true)
-		if status != j.Status {
-			// status changed, lets notify
-			state := "UP"
-			if status <= 399 && j.Status > 399 { state = "BACK UP" }
-			if status > 399 { state = "DOWN" }
-			fmt.Println("[", id, "] - ", j.Url, "is ", state, " -", status)
-			j.Status = status
-			if state == "BACK UP" || state == "DOWN" {
-				notify(state, j)
-			}
-		}
-		j.Status = status
-		results <- j.Status
-	}
+  for j := range jobs {
+    status := check_http_status(j.Url, true)
+    if status != j.Status {
+      // status changed, lets notify
+      state := "UP"
+      if status <= 399 && j.Status > 399 { state = "BACK UP" }
+      if status > 399 { state = "DOWN" }
+      fmt.Println("[", id, "] - ", j.Url, "is ", state, " -", status)
+      j.Status = status
+      if state == "BACK UP" || state == "DOWN" {
+        notify(state, j)
+      }
+    }
+    j.Status = status
+    results <- j.Status
+  }
 }
 
 /*
@@ -125,52 +125,52 @@ func worker(id int, jobs <-chan *Site, results chan<- int) {
  * Returns status code.
  */
 func check_http_status(url string, retry bool) (int) {
-	status := 408 // Something went wrong, default to 'ClientRequestTimeout'
-	transport := http.Transport{
-		Dial: dialTimeout,
-	}
+  status := 408 // Something went wrong, default to 'ClientRequestTimeout'
+  transport := http.Transport{
+    Dial: dialTimeout,
+  }
 
-	client := http.Client{
-		Transport: &transport,
-	}
-	
-	res, err := client.Get(url)
-	if err != nil && retry {
-		return check_http_status(url, false)
-	}
-	if res != nil {
-		res.Body.Close()
-		status = res.StatusCode
-	}
-	return status
+  client := http.Client{
+    Transport: &transport,
+  }
+
+  res, err := client.Get(url)
+  if err != nil && retry {
+    return check_http_status(url, false)
+  }
+  if res != nil {
+    res.Body.Close()
+    status = res.StatusCode
+  }
+  return status
 }
 
 /*
  * Dial callback for HTTP client
  */
 func dialTimeout(network, addr string) (net.Conn, error) {
-	return net.DialTimeout(network, addr, time.Duration(config.Timeout) * time.Second )
+  return net.DialTimeout(network, addr, time.Duration(config.Timeout) * time.Second )
 }
 
 func main() {
-	jobs := make(chan *Site, 100)
-	results := make(chan int, 100)
-	done := make(chan bool, 1)
+  jobs := make(chan *Site, 100)
+  results := make(chan int, 100)
+  done := make(chan bool, 1)
 
-	f, err := ioutil.ReadFile("./config.json")
-	check(err)
+  f, err := ioutil.ReadFile("./config.json")
+  check(err)
 
-	json_err := json.Unmarshal(f, &config)
-	check(json_err)
+  json_err := json.Unmarshal(f, &config)
+  check(json_err)
 
-	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
-	}
+  for w := 1; w <= 3; w++ {
+    go worker(w, jobs, results)
+  }
 
-	for s := 0; s < len(config.Sites); s++ {
-		sites = append(sites, Site{Url: config.Sites[s], Status: 0})
-	}
+  for s := 0; s < len(config.Sites); s++ {
+    sites = append(sites, Site{Url: config.Sites[s], Status: 0})
+  }
 
-	perform(jobs, results)
-	<- done
+  perform(jobs, results)
+  <- done
 }
